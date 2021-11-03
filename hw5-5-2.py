1b05a7712eff60bcfece09e21a762026e776e3f9
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-11-03 00:36:35 -0400

    Finish hw 5-5-2

--- hw5-5-2.py
+++ hw5-5-2.py
@@ -1,1 +1,9 @@
 # Author: MOG 11/2/21
+
+word = input("What is your six letter word? ")
+
+new_word1 = word[0:1] + "-" + word[2:3] + "-" + word[5:6]
+new_word2 = word[1:2] + "-" + word[3:4] + "-" + word[6:7]
+
+print(new_word1)
+print(new_word2)
