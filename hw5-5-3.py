f3f6a7676eae4d7ab65e10fab7ab8c364122024d
p22mgiroux <77790842+p22mgiroux@users.noreply.github.com>
2021-11-03 00:42:58 -0400

    Finish hw5-5-3

--- hw5-5-3.py
+++ hw5-5-3.py
@@ -1,1 +1,8 @@
 # Author: MOG 11/2/21
+
+word = input("What is your word? ")
+
+if word == word[:: -1]:
+  print("The word " + word + " is a palindrome because " + word + " spelled backwards is " + word[:: -1] + ".")
+else:
+  print("The word " + word + " is not a palindrome because " + word + " spelled backwards is " + word[:: -1] + ".")
